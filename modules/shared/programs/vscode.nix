d7c5a049f54ec3edc9ca9bf86d4d96bcf635aad5
Ujaan Das <ujaandas03@gmail.com>
2025-09-22 11:25:42 +0800

    vscode dots

--- modules/shared/programs/vscode.nix
+++ modules/shared/programs/vscode.nix
@@ -1,8 +1,8 @@
 {
   pkgs,
-  lib,
   ...
-}: {
+}:
+{
   enable = true;
   package = pkgs.vscodium;
 
@@ -14,26 +14,39 @@
       # dev
       jnoortheen.nix-ide
       eamodio.gitlens
+      usernamehw.errorlens
     ];
 
     userSettings = {
       # ui pref
       "workbench.colorTheme" = "Catppuccin Mocha";
-      "workbench.iconTheme" = "vscode-icons";
-      "workbench.sideBar.location" = "left";
-      "window.menuBarVisibility" = "classic";
+      # "workbench.iconTheme" = "vscode-icons";
+      "workbench.sideBar.location" = "right";
+      # "window.menuBarVisibility" = "classic";
+      "breadcrumbs.enabled" = false;
+      "editor.overviewRulerBorder" = false;
+      "workbench.layoutControl.enabled" = false;
+      "explorer.autoReveal" = false;
+      "editor.minimap.enabled" = false;
+      "workbench.tree.indent" = 12;
+      "workbench.tree.renderIndentGuides" = "none";
+      "workbench.editor.tabActionCloseVisibility" = false;
+      "editor.padding.bottom" = 14;
+      "editor.padding.top" = 14;
+      "editor.scrollbar.horizontal" = "hidden";
+      "editor.matchBrackets" = "never";
+      "editor.guides.highlightActiveBracketPair" = false;
+      "editor.smoothScrolling" = true;
+      "explorer.compactFolders" = false;
+      "zenMode.fullScreen" = false;
 
       # font
-      "editor.fontFamily" = "'JetBrains Mono', Consolas, 'Courier New', monospace";
+      "editor.fontFamily" = "'JetBrainsMono Nerd Font', Consolas, 'Courier New', monospace";
       "editor.fontLigatures" = true;
-      "editor.fontSize" = 14;
+      "editor.fontSize" = 13;
 
       # fmt/linting
       "editor.formatOnSave" = true;
-      "editor.codeActionsOnSave" = {
-        "source.fixAll.eslint" = true;
-        "source.organizeImports" = true;
-      };
 
       # intellisense
       "editor.quickSuggestions" = {
@@ -43,15 +56,19 @@
       };
 
       # shell
-      "terminal.integrated.fontFamily" = "JetBrains Mono";
+      "terminal.integrated.fontFamily" = "JetBrainsMono Nerd Font";
       "terminal.integrated.defaultProfile.linux" = "bash";
 
       # gitlens
       "gitlens.currentLine.enabled" = true;
       "gitlens.hovers.enabled" = true;
 
-      # misc
-      "breadcrumbs.enabled" = true;
+      # language specific
+      "nix" = {
+        "serverPath" = "nixd";
+        "enableLanguageServer" = true;
+        "serverSettings.nixd.formatting.command" = "nixfmt";
+      };
     };
   };
 }
